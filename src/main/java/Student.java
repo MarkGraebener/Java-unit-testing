90b57e035b39acd661b30845ff08aeca85281670
MarkGraebener <mark.w.graebener@gmail.com>
2022-03-14 14:41:18 -0500

    testing class added

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,2 +1,42 @@
-public class Student {
+import java.util.ArrayList;
+
+public class Student
+{
+//    id should be a long number used to represent a "unique user" in our application.
+//name is a String that holds the name of the student.
+//grades is an ArrayList that contains a list of Integer numbers.
+    private long id;
+    private  String name;
+    private ArrayList<Integer> grades;
+
+    public Student(long id, String name, ArrayList<Integer> grades) {
+        this.id = id;
+        this.name = name;
+        this.grades = grades;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Integer> getGrades() {
+        return grades;
+    }
+
+    public void setGrades(ArrayList<Integer> grades) {
+        this.grades = grades;
+    }
 }
+
