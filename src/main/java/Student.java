6fa3261fc007325a68069d66eb17c244093f006d
MarkGraebener <mark.w.graebener@gmail.com>
2022-03-16 14:45:48 -0500

    junit test

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -9,34 +9,68 @@ public class Student
     private  String name;
     private ArrayList<Integer> grades;
 
-    public Student(long id, String name, ArrayList<Integer> grades) {
+    public Student(long id, String name, ArrayList<Integer> grades)
+    {
         this.id = id;
         this.name = name;
         this.grades = grades;
     }
 
-    public long getId() {
+    public Student(long id, String name)
+    {
+        this.id = id;
+        this.name = name;
+        this.grades = new ArrayList<>();
+
+    }
+
+    public long getId()
+    {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(long id)
+    {
         this.id = id;
     }
 
-    public String getName() {
+    public String getName()
+    {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name)
+    {
         this.name = name;
     }
 
-    public ArrayList<Integer> getGrades() {
+    public ArrayList<Integer> getGrades()
+    {
         return grades;
     }
 
-    public void setGrades(ArrayList<Integer> grades) {
+    public void setGrades(ArrayList<Integer> grades)
+    {
         this.grades = grades;
     }
+    // adds the given grade to the grades list
+    public void addGrade(int grade)
+    {
+        this.grades.add(grade);
+    }
+    // returns the average of the students grades
+    public double getGradeAverage()
+    {
+        int total = 0;
+        for(int i = 0; i < this.grades.size(); i++)
+        {
+            total += this.grades.get(i);
+        }
+        return (double) total / this.grades.size();
+    }
+
+
+
+
 }
 
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,4 +1,36 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
 public class StudentTest
 {
+//    private List<String> names;
+//
+////    @Before causes that method to be run before the @Test method. The @Before methods of superclasses will be run before those of the current class. It's like a default constructor for a Plain Java Object Class, and the name of the method does not really matter, but we suggest to use setUp().
+//    @Before
+//    public void setUp(){
+//        this.names = new ArrayList<>();
+//        this.names.add("Fer");
+//    }
+//
+//    // We can verify that the names list is not longer null because it was initialized in the setUp() method with the following test:
+//    @Test
+//    public void testIfNamesIsInitialized(){
+//        assertNotNull(names);
+//    }
+//
+//    @Test
+//    public void testIfANameCanBeAdded(){
+//        assertEquals(1, this.names.size());
+//        this.names.add("Zach");
+//        assertEquals(2, this.names.size());
+//        assertSame("fer", this.names.get(0));
+//        assertSame("Zach", this.names.get(1));
+//    }
+
 
 }
